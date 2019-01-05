aa584e38dead30d4ecc69e86ffff4bd86133ec47
Unknown <inbvar@gmail.com>
2019-01-05 20:44:50 +0200

    Initial code

--- src/main.m
+++ src/main.m
@@ -1,6 +1,7 @@
-function [ output_args ] = Untitled2( input_args )
+function [ kernel ] = main( path )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
+    return
 
 
 end
