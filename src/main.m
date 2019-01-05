b139d839e097e20c30848a5b646ce21bcef4aead
Unknown <inbvar@gmail.com>
2019-01-05 20:45:21 +0200

    Create synthetic image

--- src/main.m
+++ src/main.m
@@ -1,6 +1,14 @@
 function [ kernel ] = main( path )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
+    orig = imread(path);
+    gray_image = rgb2gray(orig);  % orig(:,:,2); % takes the 'g' layer from rgb 
+    kernel = fspecial('gaussian', [5 5], 4); % symmetric 2d gaussian kernel, 5*5
+    orig_filtered = imfilter(gray_image, kernel, 'conv');
+    orig_small = orig_filtered(1:2:end,1:2:end);
+    synthetic_image = [gray_image,[orig_small;orig_small]];
+    high = double(synthetic_image);
+    
     return
 
 
